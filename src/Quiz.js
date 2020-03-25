4506fb2f042c6d6d6169c833bc02374d7bf3390e
Samir Kumar Adak <samir.adak@mindtree.com>
2020-03-25 14:36:51 +0530

    Module 4 completed

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import QuizQuestion from './QuizQuestion'
+import QuizEnd from './QuizEnd';
 
 let quizData = require('./quiz_data.json');
 
@@ -8,13 +9,21 @@ class Quiz extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            quiz_position: 1 
+            quiz_position: 3
         }
     }
     render() {
-        return (
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
-        )
+        const isQuizEnd = quizData.quiz_questions.length === (this.state.quiz_position - 1) ? true : false;
+        console.log(isQuizEnd, quizData.quiz_questions.length)
+        if(isQuizEnd === true) {
+            return(<QuizEnd />)
+        } 
+        if(isQuizEnd === false) {
+            return (<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />)
+        }
+        // return (
+        //     isQuizEnd ? (<QuizEnd />) : (<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />)
+        // )
     }
 }
 
