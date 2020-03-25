010295a689f68f00d8a241a28447357aeb0df836
Samir Kumar Adak <samir.adak@mindtree.com>
2020-03-25 12:02:27 +0530

    QuizQuestion module completed

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import QuizQuestion from './QuizQuestion'
+
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
@@ -8,13 +10,10 @@ class Quiz extends Component {
         this.state = {
             quiz_position: 1 
         }
-        console.log(quizData);
     }
     render() {
         return (
-            <div>
-                <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
-            </div>
+            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
         )
     }
 }
